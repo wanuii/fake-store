e8b307e3da203ca833aa0ff07ec6a43a66d2a34e
wanuii <137756091+wanuii@users.noreply.github.com>
2025-01-08 01:21:12 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
   plugins: [
     vue(),
   ],
-  base: './shopping-cart',
+  base: '/shopping-cart/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
